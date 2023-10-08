b3f9e53f5e5d9e587307b522e554f11a31f70ca8
1134701187@qq.com <1134701187@qq.com>
2023-10-08 14:28:15 +0800

    first commit

---
+++ pages/music/music.js
@@ -0,0 +1,90 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    gridList: [{
+        path: "/images/tab-search.png",
+        name: "搜索"
+      },
+      {
+        path: "/images/tab-video.png",
+        name: "精选视频"
+      },
+      {
+        path: "/images/tab-hot.png",
+        name: "热门歌曲"
+      },
+      {
+        path: "/images/tab-type.png",
+        name: "分类"
+      }, {
+        path: "/images/tab-my.png",
+        name: "个人中心"
+      }
+    ],
+    swiperList: [
+      " https://p1.music.126.net/-tRQx8RHsLITycqit3fZCQ==/109951168817731818.jpg",
+      "https://p1.music.126.net/6zU--t7MTtX7ivuDMJmYKQ==/109951168817747553.jpg",
+      "https://p1.music.126.net/xXznayH8ascRKweLw6xgcw==/109951168817739991.jpg"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
